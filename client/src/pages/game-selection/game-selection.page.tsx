139a565828b095965d8c2f4fde6f828817314bb6
Paolo Giua <paolo.giuaa@gmail.com>
2024-04-20 18:01:41 +0200

    Added form validation in NicknamePage and GameSelectionPage

--- client/src/pages/game-selection/game-selection.page.tsx
+++ client/src/pages/game-selection/game-selection.page.tsx
@@ -3,13 +3,33 @@ import './game-selection.page.scss';
 import { Col, Container, Row } from 'react-bootstrap';
 
 interface GameSelectionPageState {
+  gameCode: string,
+  validity: ValidityState,
   errorMessage?: string;
 }
 
 class GameSelectionPage extends Component {
   state: Readonly<GameSelectionPageState> = {
+    gameCode: '',
+    validity: {} as ValidityState,
     errorMessage: undefined,
   };
+
+  onSubmit(): void {
+    if (this.state.validity.tooShort) {
+      this.setState({
+        errorMessage: 'Testo troppo corto',
+      });
+    }
+  }
+
+  onInputChange({ target }: { target: HTMLInputElement }): void {
+    this.setState({
+      nickname: target.value,
+      validity: target.validity,
+      errorMessage: undefined,
+    });
+  }
 
   render(): React.ReactNode {
     return (
@@ -28,12 +48,15 @@ class GameSelectionPage extends Component {
                     className={`nes-input is-dark ${this.state.errorMessage ? 'is-error' : ''}`}
                     autoComplete='off'
                     placeholder='#56729'
+                    minLength={5}
+                    maxLength={6}
+                    onChange={e => this.onInputChange(e)}
                   />
                 </Col>
               </Row>
             </Col>
             <Col xs='12' md='auto'>
-              <button className='nes-btn is-success'>Ok</button>
+              <button className='nes-btn is-success' onClick={_ => this.onSubmit()}>Ok</button>
             </Col>
             <Col xs='12' hidden={!this.state.errorMessage}>
               <span className='d-flex justify-content-center nes-text is-error'>
--- client/src/pages/nickname/nickname.page.tsx
+++ client/src/pages/nickname/nickname.page.tsx
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, FormEvent, useState } from 'react';
 import './nickname.page.scss';
 import { Col, Container, Row } from 'react-bootstrap';
 
 interface NicknamePageState {
+  nickname: string,
+  validity: ValidityState,
   errorMessage?: string;
 }
 
 class NicknamePage extends Component {
   state: Readonly<NicknamePageState> = {
+    nickname: '',
+    validity: {} as ValidityState,
     errorMessage: undefined,
   };
+
+  onSubmit(): void {
+    if (this.state.validity.tooShort) {
+      this.setState({
+        errorMessage: 'Testo troppo corto',
+      });
+    }
+  }
+
+  onInputChange({ target }: { target: HTMLInputElement }): void {
+    this.setState({
+      nickname: target.value,
+      validity: target.validity,
+      errorMessage: undefined,
+    });
+  }
 
   render(): React.ReactNode {
     return (
@@ -22,18 +42,21 @@ class NicknamePage extends Component {
                   <label htmlFor='input'>Nickname</label>
                 </Col>
                 <Col xs='12' md='auto'>
-                  <input 
-                    type='text' 
-                    id='input' 
-                    className={`nes-input is-dark ${this.state.errorMessage ? 'is-error' : ''}`} 
-                    autoComplete='off' 
-                    placeholder='ex. BATMAN' 
+                  <input
+                    type='text'
+                    id='input'
+                    className={`nes-input is-dark ${this.state.errorMessage ? 'is-error' : ''}`}
+                    autoComplete='off'
+                    placeholder='ex. BATMAN'
+                    minLength={3}
+                    maxLength={15}
+                    onChange={e => this.onInputChange(e)}
                   />
                 </Col>
               </Row>
             </Col>
             <Col xs='12' md='auto'>
-              <button className='nes-btn is-success'>Ok</button>
+              <button className='nes-btn is-success' onClick={_ => this.onSubmit()}>Ok</button>
             </Col>
             <Col xs='12' hidden={!this.state.errorMessage}>
               <span className='d-flex justify-content-center nes-text is-error'>
