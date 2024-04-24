4caadeccb06b23f26b1b4f00ef1aa630b6036c36
Paolo Giua <p.giua@reply.it>
2024-04-24 19:47:08 +0200

    Now pacman can move!

--- client/src/components/phaser-game/main.ts
+++ client/src/components/phaser-game/main.ts
@@ -1,17 +1,20 @@
-import Phaser from 'phaser';
-import { GameScene } from './scenes/game.scene';
+import Phaser from "phaser";
+import { GameScene } from "./scenes/game.scene";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 432,
-	height: 368,
-	parent: 'game-container',
-	backgroundColor: '#000000',
-	scene: GameScene,
+  type: Phaser.AUTO,
+  width: 432,
+  height: 368,
+  parent: "game-container",
+  backgroundColor: "#000000",
+  physics: {
+    default: "arcade",
+  },
+  scene: GameScene,
 };
 
 const StartGame = (): Phaser.Game => {
-	return new Phaser.Game(config);
-}
+  return new Phaser.Game(config);
+};
 
 export default StartGame;
--- client/src/components/phaser-game/scenes/game.scene.ts
+++ client/src/components/phaser-game/scenes/game.scene.ts
@@ -1,28 +1,95 @@
 import Phaser from "phaser";
 import { FIRST_MAP } from "../maps/first.map";
-const imageContext = require.context('../../../images/map/', true);
-const images = imageContext.keys().map(image => imageContext(image));
+import pacman from "../../../images/spritesheets/pacman.png";
+const imageContext = require.context("../../../images/map/", true);
+const images = imageContext.keys().map((image) => imageContext(image));
 
 export class GameScene extends Phaser.Scene {
+  private _cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private _platforms!: Phaser.Physics.Arcade.StaticGroup;
+  private _player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
   // constructor() {
   //   super();
   // }
 
-  init(): void {}
+  init(): void {
+    this._cursors = this.input.keyboard!.createCursorKeys();
+  }
 
   preload(): void {
-		images.forEach((image: string, imageId: number) => 
-			this.load.image(`map-${imageId}`, image)
-		);
+    this.load.spritesheet("pacman", pacman, {frameWidth: 16, frameHeight: 16});
+    images.forEach((image: string, imageId: number) =>
+      this.load.image(`map-${imageId}`, image)
+    );
   }
 
   create(): void {
-		FIRST_MAP.forEach((row, i) => 
-			row.forEach((imageId, j) => 
-				imageId !== -1 && this.add.image(j*16 + 8, i*16 + 8, `map-${imageId}`)
-			)
-		);
+    this.anims.create({
+      key: "down",
+      frames: this.anims.generateFrameNumbers("pacman", { start: 1, end: 2 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("pacman", { start: 3, end: 4 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("pacman", { start: 5, end: 6 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "up",
+      frames: this.anims.generateFrameNumbers("pacman", { start: 7, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this._platforms = this.physics.add.staticGroup();
+
+    FIRST_MAP.forEach((row, i) =>
+      row.forEach((imageId, j) => {
+        if (imageId !== -1) {
+          let x: number = j * 16 + 8;
+          let y: number = i * 16 + 8;
+
+          if (imageId < 14) {
+            this._platforms.create(x, y, `map-${imageId}`);
+          } else {
+            this.add.image(x, y, `map-${imageId}`);
+          }
+        }
+      })
+    );
+
+    this._player = this.physics.add.sprite(216, 216, "pacman");
+    this._player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(this._player, this._platforms);
   }
 
-  update(): void {}
+  update(): void {
+    if (this._cursors.left.isDown) {
+      this._player.setVelocityX(-160);
+
+      this._player.anims.play("left", true);
+    } else if (this._cursors.right.isDown) {
+      this._player.setVelocityX(160);
+
+      this._player.anims.play("right", true);
+    } else if (this._cursors.up.isDown) {
+      this._player.setVelocityY(-160);
+
+      this._player.anims.play("up", true);
+    } else if (this._cursors.down.isDown) {
+      this._player.setVelocityY(160);
+
+      this._player.anims.play("down", true);
+    }
+  }
 }
